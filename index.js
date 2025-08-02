20829993469ce5cb88e7678e0c344768b51901f1
juanpsp <juanplg1308@gmail.com>
2025-08-02 14:39:07 -0300

    commite com o resultado do projeto.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello, World!");
+console.log("DESAFIO: NÍVEL DO HERÓI");
+
+function determinarNivel(nivel) {
+  if (nivel <= 1000) {
+    return "Ferro";
+  } else if (nivel <= 2000) {
+    return "Bronze";
+  } else if (nivel <= 5000) {
+    return "Prata Ouro";
+  } else if (nivel <= 8000) {
+    return "Platina Diamante";
+  } else if (nivel <= 9000) {
+    return "Ascendente";
+  } else if (nivel <= 10000) {
+    return "Imortal";
+  } else {
+    return "Radiante";
+  }
+}
+
+let nivelHeroi = 1313;
+console.log("O nível do seu herói é: " + determinarNivel(nivelHeroi));
